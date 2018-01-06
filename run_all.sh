57b3dd4753fcae875396e9b0a51966a48c53355d
Josh Wright <madewithlinux.josh@gmail.com>
2018-01-05 20:53:37 -0600

    reorganize files, fix imports for new project repo

--- run_all.sh
+++ run_all.sh
@@ -14,20 +14,18 @@ mains=(
 	test_bspline
 )
 
-mkdir -p build/bin
+mkdir -p build
 cd build
 
 echo > run_log.txt
 for main in ${mains[@]}; do
 	echo $main
-	cd bin
-	if ! go build "../../main/${main}.go"; then
+	if ! go build ../main/${main}.go; then
 		echo "${main} failed to build"
 		echo "${main} failed to build" >> run_log.txt
 		continue
 	fi
-	cd ..
-	if ! "./bin/${main}"; then
+	if ! ./${main}; then
 		echo "${main} failed to run"
 		echo "${main} failed to run" >> run_log.txt
 		continue
