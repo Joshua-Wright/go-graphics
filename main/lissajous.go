68edc14d46c9ae649a5a90a960cbee2ec1f74a31
Josh Wright <madewithlinux.josh@gmail.com>
2018-01-14 16:45:35 -0600

    maurer rose and maurer lissajous

--- main/lissajous.go
+++ main/lissajous.go
@@ -15,26 +15,25 @@ func main() {
 	height := 2048
 	nPts := 256
 	overstep := 32
-	n := 2.0
-	d := 3.0
+	a := 4.0
+	b := 3.0
 	linewidth := 2.0
 	blur_radius := 8 * linewidth
-	maxTheta := (2 * math.Pi) * 3
+	maxTheta := (2 * math.Pi)
 	bounds := [4]g.Float{
 		// a little extra space around the rose
 		-1.1, 1.1,
 		-1.1, 1.1,
 	}
 
-	k := float64(n) / float64(d)
 	pts := make([]g.Vec2, nPts)
 	for i := 0; i < nPts; i++ {
 		theta := float64(i) / float64(nPts) * maxTheta
 		x, y := g.WindowTransformPoint(
 			width, height,
 			g.Vec2{
-				math.Cos(k*theta) * math.Cos(theta),
-				math.Cos(k*theta) * math.Sin(theta)},
+				math.Sin(a * theta),
+				math.Cos(b * theta)},
 			bounds)
 		pts[i] = g.Vec2{float64(x), float64(y)}
 	}
