762c32d61bf3f565d2bfb396ee69060f1f4041bb
Josh Wright <madewithlinux.josh@gmail.com>
2018-03-22 19:16:27 -0500

    mmap ppm to png conversion

---
+++ main/ppm_to_png.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/joshua-wright/go-graphics/graphics/file_backed_image"
+	"os"
+	"strings"
+	"path/filepath"
+	"image/png"
+)
+
+func main() {
+	filename := os.Args[1]
+	newFilename := strings.TrimSuffix(filename, filepath.Ext(filename)) + ".png"
+
+	img, err := file_backed_image.OpenPPM(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(newFilename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	err = png.Encode(file, img)
+	if err != nil {
+		panic(err)
+	}
+}
