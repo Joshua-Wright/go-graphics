1197825c92d6cbf26ff57bde743a78a0a4d116c4
Josh Wright <madewithlinux.josh@gmail.com>
2018-03-29 22:37:38 -0500

    clifford fractal with squished histogram normalization

--- main/clifford/clifford_histogram_normalization.go
+++ main/clifford/clifford_histogram_normalization.go
@@ -24,17 +24,16 @@ func main() {
 	c := -1.84466019417476
 	d := 1.12621359223301
 
-	overbleed := 8.0
+	//overbleed := 8.0
 
 	nPreIter := 400
-	nIter := 20000000
+	nIter := 1<<27
 
 	width := 2560
 	height := 1440
 
 	bound_width := 3.0
 	bounds := [4]g.Float{
-		// a little extra space around the rose
 		-bound_width, bound_width,
 		-bound_width, bound_width,
 		//-bound_width * 9.0 / 16.0, bound_width * 9.0 / 16.0,
@@ -46,12 +45,10 @@ func main() {
 	for i := 0; i < len(buffers); i++ {
 		buffers[i] = Slice2D.NewUint16Slice2D(width, height)
 	}
-	maxes := make([]uint16, nProcs)
 
-	// iterate points
+	println("iterate points")
 	parallel.ParallelFor(0, nProcs, func(workerIndex int) {
 		counts := buffers[workerIndex]
-		var max_count uint16
 
 		pt := g.Vec2{
 			X: rand.Float64()*2 - 1,
@@ -68,48 +65,80 @@ func main() {
 				println(x, y)
 			}
 			newCount := counts.Get(x, y) + 1
+			if newCount == math.MaxUint16 {
+				panic("too big")
+			}
 			counts.Set(x, y, newCount)
-			if newCount > max_count {
-				max_count = newCount
+		}
+	})
+
+	println("histogram normalize")
+	cdfs := make([][]uint64, nProcs)
+	parallel.ParallelFor(0, nProcs, func(i int) {
+		cdfs[i] = make([]uint64, math.MaxUint16+1)
+		cdf := cdfs[i]
+
+		// count non-zeroes
+		for _, e := range buffers[i].Data {
+			if e != 0 {
+				cdf[e]++
 			}
 		}
 
-		maxes[workerIndex] = max_count
+		// prefix sum to make it a cdf
+		for i := 1; i < math.MaxUint16+1; i++ {
+			cdf[i] += cdf[i-1]
+		}
 	})
-
-	// find max point count
-	maxCount := 0.0
-	for _, newCount := range maxes {
-		if float64(newCount) > maxCount {
-			maxCount = float64(newCount)
+	// reduce to single cdf
+	cdf := make([]uint64, math.MaxUint16+1)
+	for i := 0; i < math.MaxUint16+1; i++ {
+		cdf[i] = 0
+		for j := 0; j < nProcs; j++ {
+			cdf[i] += cdfs[j][i]
 		}
 	}
+	numNonEmptyPixels := cdf[math.MaxUint16]
 
 	out := image.NewNRGBA(image.Rect(0, 0, width, height))
 
-	// add and normalize points
+	println("add and normalize points")
 	parallel.ParallelForAdaptive(0, width, func(startInclusive, endExclusive int) {
 		for x := startInclusive; x < endExclusive; x++ {
 			for y := 0; y < height; y++ {
 
 				// sum contribution from each processor
-				count := 0.0
+				counti := uint16(0)
 				for i := 0; i < nProcs; i++ {
-					count += float64(buffers[i].Get(x, y))
+					counti += buffers[i].Get(x, y)
+				}
+				if counti == 0 {
+					out.Set(x, y, color.White)
+				} else {
+					count := float64(cdf[counti]-1) / float64(numNonEmptyPixels-1)
+					// squish the colors a lot
+					count = 1 - math.Pow(1-count, 1.0/6.0)
+					if count < 0 || count > 1.0 {
+						// bad
+						println(count)
+					}
+					count *= 255
+					//println(int(count))
+
+					// normalize
+					//count = count / maxCount * 255 * overbleed
+					//if count > 255 {
+					//	count = 255
+					//}
+
+					// set element in output image
+					out.Set(x, y, color.Gray{255 - uint8(count)})
 				}
 
-				// normalize
-				count = count / maxCount * 255 * overbleed
-				if count > 255 {
-					count = 255
-				}
-
-				// set element in output image
-				out.Set(x, y, color.Gray{255 - uint8(count)})
 			}
 		}
 	})
 
+	println("save png")
 	g.SaveAsPNG(out, g.ExecutableNamePng())
-
 }
