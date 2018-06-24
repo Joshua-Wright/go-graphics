62b66216de4d26de836096e18dde58b724d29232
Josh Wright <madewithlinux.josh@gmail.com>
2018-06-24 18:40:22 -0500

    aws lambda range endpoint

--- graphics/mandelbrot_fixed_point/types.go
+++ graphics/mandelbrot_fixed_point/types.go
@@ -20,3 +20,28 @@ type MandelbrotSinglePixelResponse struct {
 	Val       float64 `json:"val"`
 	Mag2      string  `json:"mag2"`
 }
+
+type MandelbrotPixelRangeConfig struct {
+	Imin            int64  `json:"imin"`
+	Imax            int64  `json:"imax"`
+	Jmin            int64  `json:"jmin"`
+	Jmax            int64  `json:"jmax"`
+	Width           int64  `json:"width"`
+	Height          int64  `json:"height"`
+	MaxIter         int64  `json:"maxIter"`
+	CenterR         string `json:"centerR"`
+	CenterI         string `json:"centerI"`
+	Zoom            string `json:"zoom"`
+	Threshold2      string `json:"threshold2"`
+	BasePower2      uint   `json:"basePower2"`
+	ReturnIteration bool   `json:"returnIteration"`
+	ReturnVal       bool   `json:"returnVal"`
+	ReturnMag2      bool   `json:"returnMag2"`
+}
+
+// if one of these wasn't requested, return empty array instead
+type MandelbrotSPixelRangeResponse struct {
+	Iteration []int64   `json:"iteration"`
+	Val       []float64 `json:"val"`
+	Mag2      []string  `json:"mag2"`
+}
--- lambda/mandelbrot_fixed_point.go
+++ lambda/mandelbrot_fixed_point.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
 	m "github.com/joshua-wright/go-graphics/graphics/mandelbrot_fixed_point"
 	"encoding/json"
 	"github.com/ncw/gmp"
 	"log"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func MandelbrotPixel(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var cfg m.MandelbrotSinglePixelConfig
+	var cfg m.MandelbrotPixelRangeConfig
 	err := json.Unmarshal([]byte(request.Body), &cfg)
+	//decoder := gob.NewDecoder(bytes.NewBuffer([]byte(request.Body)))
+	//err := decoder.Decode(&cfg)
 
 	if err != nil {
 		log.Println("bad json: ", request.Body)
@@ -51,16 +53,40 @@ func MandelbrotPixel(request events.APIGatewayProxyRequest) (events.APIGatewayPr
 		}, err
 	}
 
-	cr, ci := m.MandelbrotCoordinate(cfg.I, cfg.J, cfg.Width, cfg.Height, centerR, centerI, zoom, cfg.BasePower2)
+	iRange := cfg.Imax - cfg.Imin
+	jRange := cfg.Jmax - cfg.Jmin
+	size := iRange * jRange
 
-	iteration, val, mag2 := m.MandelbrotKernel(cr, ci, threshold2, cfg.MaxIter, cfg.BasePower2)
+	resp := m.MandelbrotSPixelRangeResponse{}
+	if cfg.ReturnIteration {
+		resp.Iteration = make([]int64, size)
+	}
+	if cfg.ReturnVal {
+		resp.Val = make([]float64, size)
+	}
+	if cfg.ReturnMag2 {
+		resp.Mag2 = make([]string, size)
+	}
 
-	resp := m.MandelbrotSinglePixelResponse{
-		I:         cfg.I,
-		J:         cfg.J,
-		Iteration: iteration,
-		Val:       val,
-		Mag2:      mag2.String(),
+	for i0 := int64(0); i0 < iRange; i0++ {
+		for j0 := int64(0); j0 < jRange; j0++ {
+			i := i0 + cfg.Imin
+			j := j0 + cfg.Jmin
+
+			cr, ci := m.MandelbrotCoordinate(i, j, cfg.Width, cfg.Height, centerR, centerI, zoom, cfg.BasePower2)
+			iteration, val, mag2 := m.MandelbrotKernel(cr, ci, threshold2, cfg.MaxIter, cfg.BasePower2)
+
+			idx := iRange*j0 + i0
+			if cfg.ReturnIteration {
+				resp.Iteration[idx] = iteration
+			}
+			if cfg.ReturnVal {
+				resp.Val[idx] = val
+			}
+			if cfg.ReturnMag2 {
+				resp.Mag2[idx] = mag2.String()
+			}
+		}
 	}
 
 	respStr, err := json.Marshal(resp)
@@ -79,4 +105,10 @@ func MandelbrotPixel(request events.APIGatewayProxyRequest) (events.APIGatewayPr
 func main() {
 	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(MandelbrotPixel)
+	//resp, err := MandelbrotPixel(events.APIGatewayProxyRequest{
+	//	//Body:`{"i": 0,"j": 0,"width": 1024,"height": 1024,"maxIter": 5120,"centerR": "-0.74364085","centerI": "0.13182733","zoom": "120188","threshold2": "32.0","basePower2": 128}`,
+	//	//Body: `{"imin": 0,"imax": 64,"jmin": 0,"jmax": 1,"width": 64,"height": 64,"maxIter": 5120,"centerR": "-0.74364085","centerI": "0.13182733","zoom": "120188","threshold2": "1024.0","basePower2": 128,"returnIteration": false,"returnVal": true,"returnMag2": false}`,
+	//	Body: `{"imin": 0,"imax": 64,"jmin": 0,"jmax": 1,"width": 64,"height": 64,"maxIter": 5120,"centerR": "0.0","centerI": "0.0","zoom": "1","threshold2": "1024.0","basePower2": 128,"returnIteration": false,"returnVal": true,"returnMag2": false}`,
+	//})
+	//fmt.Println(resp, err)
 }
