a18d22398a2d8aa3655d9f727200a69704096ee4
Josh Wright <madewithlinux.josh@gmail.com>
2018-07-04 16:15:51 -0500

    figured out how to use variable precision in mathematica

--- main/mandelbrot_fixed_point/mandelbrot_coordinate.go
+++ main/mandelbrot_fixed_point/mandelbrot_coordinate.go
@@ -15,33 +15,53 @@ func main() {
 		panic("too many args")
 	}
 	basePower2 := uint(parseInt64(os.Args[3]))
-	zoom, success := new(gmp.Int).SetString(os.Args[6], 10)
-	if !success {
-		panic("bad zoom")
-	}
 	x := parseInt64(os.Args[7])
 	y := parseInt64(os.Args[8])
-	w := parseInt64(os.Args[1])
-	h := parseInt64(os.Args[2])
-	centerR := mandelbrot.ParseFixnum(os.Args[4], basePower2)
-	centerI := mandelbrot.ParseFixnum(os.Args[5], basePower2)
+	width := parseInt64(os.Args[1])
+	height := parseInt64(os.Args[2])
+	centerR := os.Args[4]
+	centerI := os.Args[5]
+	zoom := os.Args[6]
+
+	decimalDigits := 128
+
+	///
+	centerRgmp := mandelbrot.ParseFixnum(centerR, basePower2)
+	centerIgmp := mandelbrot.ParseFixnum(centerI, basePower2)
+	zoomGmp, success := new(gmp.Int).SetString(zoom, 10)
+	if !success {
+		panic("bad zoomGmp")
+	}
 	cr, ci := mandelbrot.MandelbrotCoordinate(
-		x, y, w, h,
-		centerR, centerI,
-		zoom, basePower2)
+		x, y, width, height,
+		centerRgmp, centerIgmp,
+		zoomGmp, basePower2)
 
 	fmt.Println("cr =", cr.String(), "/2^", basePower2)
 	fmt.Println("ci =", ci.String(), "/2^", basePower2)
 
 	cr1, _ := mandelbrot.MandelbrotCoordinate(
-		0, 0, w, h,
-		centerR, centerI,
-		zoom, basePower2)
+		0, 0, width, height,
+		centerRgmp, centerIgmp,
+		zoomGmp, basePower2)
 	cr2, _ := mandelbrot.MandelbrotCoordinate(
-		1, 0, w, h,
-		centerR, centerI,
-		zoom, basePower2)
-	fmt.Println("pixel width = (", cr1.String(), "-", cr2.String(), ")/2^", basePower2)
+		1, 0, width, height,
+		centerRgmp, centerIgmp,
+		zoomGmp, basePower2)
+	fmt.Println("pixel width = (", cr1.String(), " - ", cr2.String(), ")/2^", basePower2)
+
+	// mathematica-flavored output
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("decimalDigits=", decimalDigits)
+	fmt.Println("decimal[x_] := DecimalForm[N[x,decimalDigits],decimalDigits]")
+	fmt.Println("cr := ", cr.String(), "/2^", basePower2)
+	fmt.Println("ci := ", ci.String(), "/2^", basePower2)
+	fmt.Println("dx := (", cr1.String(), " - ", cr2.String(), ")/2^", basePower2)
+	fmt.Println("decimal[cr]")
+	fmt.Println("decimal[ci]")
+	fmt.Println("decimal[dx]")
+	fmt.Println()
 }
 
 func parseInt64(str string) int64 {
